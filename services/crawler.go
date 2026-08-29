db6bf2736b2e57f3259c3f120b7c28120067cd31
Daniel Avshalumov <avshalumov.daniel@Daniels-MacBook-Pro.local>
2026-08-29 19:40:45 -0400

    memes

--- services/crawler.go
+++ services/crawler.go
@@ -762,7 +762,7 @@ func (cw *CrawlerWorker) extractAnchorTags(page_url string, proxyFlag bool, s *m
 				delete(cw.tabs, id)
 				cw.crawlmu.Unlock()
 			}(id)
-			fmt.Println("Run started")
+			fmt.Println("Run starts")
 			err := chromedp.Run(tabContext,
 				chromedp.Navigate(page_url),
 				// 		chromedp.ActionFunc(func(ctx context.Context) error {
